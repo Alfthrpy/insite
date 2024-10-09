7d9e950fdf8f22a7cf227bb766faf694c8f74cb3
Al-Fathir <118950136+Alfthrpy@users.noreply.github.com>
2024-10-09 16:49:11 +0700

    fix : migration

--- prisma/migrations/20241009093913_init_migrate/migration.sql
+++ prisma/migrations/20241009093913_init_migrate/migration.sql
@@ -9,23 +9,23 @@ CREATE TYPE "confirmation_status" AS ENUM ('pending', 'confirmed', 'declined');
 
 -- CreateTable
 CREATE TABLE "musics" (
-    "id" UUID NOT NULL,
-    "invitationId" VARCHAR(255) NOT NULL,
-    "musicUrl" VARCHAR(255) NOT NULL,
-    "createdAt" TIMESTAMP(0) NOT NULL,
-    "updatedAt" TIMESTAMP(0) NOT NULL,
-    "deletedAt" TIMESTAMP(0) NOT NULL,
+    "id" TEXT NOT NULL,
+    "invitationId" TEXT NOT NULL,
+    "musicUrl" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "musics_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "comentars" (
-    "id" UUID NOT NULL,
-    "invitationId" VARCHAR(255) NOT NULL,
+    "id" TEXT NOT NULL,
+    "invitationId" TEXT NOT NULL,
     "text" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(0) NOT NULL,
-    "updatedAt" TIMESTAMP(0) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
     "deletedAt" BIGINT NOT NULL,
 
     CONSTRAINT "comentars_pkey" PRIMARY KEY ("id")
@@ -33,193 +33,193 @@ CREATE TABLE "comentars" (
 
 -- CreateTable
 CREATE TABLE "brides and grooms" (
-    "id" UUID NOT NULL,
-    "invitationId" VARCHAR(255) NOT NULL,
-    "nameGroom" VARCHAR(255) NOT NULL,
-    "imageGroom" VARCHAR(255) NOT NULL,
-    "parentGroom" VARCHAR(255) NOT NULL,
-    "linkInstagramGroom" VARCHAR(255),
-    "linkFbGroom" VARCHAR(255),
-    "linkTwitterGroom" VARCHAR(255),
-    "linkYtbGroom" VARCHAR(255),
-    "nameBride" VARCHAR(255) NOT NULL,
-    "imageBride" VARCHAR(255) NOT NULL,
-    "parentBride" VARCHAR(255) NOT NULL,
-    "linkInstagramBride" VARCHAR(255),
-    "linkFbBride" VARCHAR(255),
-    "linkTwitterBride" VARCHAR(255),
-    "linkYtbBride" VARCHAR(255),
-    "createdAt" TIMESTAMP(0) NOT NULL,
-    "updatedAt" TIMESTAMP(0) NOT NULL,
-    "deletedAt" TIMESTAMP(0) NOT NULL,
+    "id" TEXT NOT NULL,
+    "invitationId" TEXT NOT NULL,
+    "nameGroom" TEXT NOT NULL,
+    "imageGroom" TEXT NOT NULL,
+    "parentGroom" TEXT NOT NULL,
+    "linkInstagramGroom" TEXT,
+    "linkFbGroom" TEXT,
+    "linkTwitterGroom" TEXT,
+    "linkYtbGroom" TEXT,
+    "nameBride" TEXT NOT NULL,
+    "imageBride" TEXT NOT NULL,
+    "parentBride" TEXT NOT NULL,
+    "linkInstagramBride" TEXT,
+    "linkFbBride" TEXT,
+    "linkTwitterBride" TEXT,
+    "linkYtbBride" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "brides and grooms_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "events" (
-    "id" UUID NOT NULL,
-    "invitationId" VARCHAR(255) NOT NULL,
-    "nameEvent" VARCHAR(255) NOT NULL,
-    "location" VARCHAR(255) NOT NULL,
-    "address" VARCHAR(255) NOT NULL,
-    "dateEvent" DATE NOT NULL,
-    "startTime" TIME NOT NULL,
-    "endTime" TIME NOT NULL,
-    "linkNavigationMap" VARCHAR(255) NOT NULL,
-    "createdAt" TIMESTAMP(0) NOT NULL,
-    "updatedAt" TIMESTAMP(0) NOT NULL,
-    "deletedAt" TIMESTAMP(0) NOT NULL,
+    "id" TEXT NOT NULL,
+    "invitationId" TEXT NOT NULL,
+    "nameEvent" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "dateEvent" TIMESTAMP(3) NOT NULL,
+    "startTime" TIMESTAMP(3) NOT NULL,
+    "endTime" TIMESTAMP(3) NOT NULL,
+    "linkNavigationMap" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "events_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "payment_transactions" (
-    "id" UUID NOT NULL,
-    "invitationId" VARCHAR(255) NOT NULL,
-    "paymentMethod" VARCHAR(255) NOT NULL,
-    "amount" DECIMAL(8,2) NOT NULL,
-    "paymentStatus" VARCHAR(255) NOT NULL DEFAULT 'pending',
-    "transactionDate" TIMESTAMP(0) NOT NULL,
+    "id" TEXT NOT NULL,
+    "invitationId" TEXT NOT NULL,
+    "paymentMethod" TEXT NOT NULL,
+    "amount" DECIMAL(65,30) NOT NULL,
+    "paymentStatus" TEXT NOT NULL DEFAULT 'pending',
+    "transactionDate" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "payment_transactions_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "rsvps" (
-    "id" UUID NOT NULL,
-    "invitationId" VARCHAR(255) NOT NULL,
-    "guestName" VARCHAR(255) NOT NULL,
+    "id" TEXT NOT NULL,
+    "invitationId" TEXT NOT NULL,
+    "guestName" TEXT NOT NULL,
     "numberOfPeople" BIGINT NOT NULL,
-    "confirmationStatus" VARCHAR(255) NOT NULL,
-    "createdAt" TIMESTAMP(0) NOT NULL,
-    "updatedAt" TIMESTAMP(0) NOT NULL,
-    "deletedAt" TIMESTAMP(0) NOT NULL,
+    "confirmationStatus" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "rsvps_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "invitations" (
-    "id" UUID NOT NULL,
-    "userId" VARCHAR(255) NOT NULL,
-    "designId" VARCHAR(255) NOT NULL,
-    "link" VARCHAR(255) NOT NULL,
-    "createdAt" TIMESTAMP(0) NOT NULL,
-    "updatedAt" TIMESTAMP(0) NOT NULL,
-    "deletedAt" TIMESTAMP(0) NOT NULL,
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "designId" TEXT NOT NULL,
+    "link" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "invitations_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Quotes" (
-    "id" UUID NOT NULL,
-    "invitationId" VARCHAR(255) NOT NULL,
+    "id" TEXT NOT NULL,
+    "invitationId" TEXT NOT NULL,
     "content" TEXT NOT NULL,
-    "author" VARCHAR(255) NOT NULL,
-    "createdAt" TIMESTAMP(0) NOT NULL,
-    "updatedAt" TIMESTAMP(0) NOT NULL,
-    "deletedAt" TIMESTAMP(0) NOT NULL,
+    "author" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Quotes_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "designs" (
-    "id" UUID NOT NULL,
-    "name" VARCHAR(255) NOT NULL,
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
     "price" DOUBLE PRECISION NOT NULL,
-    "imageUrl" VARCHAR(255) NOT NULL,
-    "templateFile" VARCHAR(255) NOT NULL,
-    "category" VARCHAR(255) NOT NULL,
-    "createdAt" TIMESTAMP(0) NOT NULL,
-    "updatedAt" TIMESTAMP(0) NOT NULL,
-    "deletedAt" TIMESTAMP(0) NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "templateFile" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "designs_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "gifts" (
-    "id" UUID NOT NULL,
-    "invitationId" VARCHAR(255) NOT NULL,
-    "nameAccount" VARCHAR(255) NOT NULL,
-    "noAccount" VARCHAR(255) NOT NULL,
-    "imgAccount" VARCHAR(255),
-    "createdAt" TIMESTAMP(0) NOT NULL,
-    "updatedAt" TIMESTAMP(0) NOT NULL,
-    "deletedAt" TIMESTAMP(0) NOT NULL,
+    "id" TEXT NOT NULL,
+    "invitationId" TEXT NOT NULL,
+    "nameAccount" TEXT NOT NULL,
+    "noAccount" TEXT NOT NULL,
+    "imgAccount" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "gifts_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "love_storys" (
-    "id" UUID NOT NULL,
-    "invitationId" VARCHAR(255) NOT NULL,
-    "title" VARCHAR(255) NOT NULL,
+    "id" TEXT NOT NULL,
+    "invitationId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
     "story" TEXT NOT NULL,
-    "imageUrl" VARCHAR(255),
-    "createdAt" TIMESTAMP(0) NOT NULL,
-    "updatedAt" TIMESTAMP(0) NOT NULL,
-    "deletedAt" TIMESTAMP(0) NOT NULL,
+    "imageUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "love_storys_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "settings" (
-    "id" UUID NOT NULL,
-    "invitationId" VARCHAR(255) NOT NULL,
-    "title" VARCHAR(255) NOT NULL,
+    "id" TEXT NOT NULL,
+    "invitationId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
     "textPembuka" TEXT NOT NULL,
     "textAcara" TEXT NOT NULL,
     "textPenutup" TEXT NOT NULL,
     "broadcast" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(0) NOT NULL,
-    "updatedAt" TIMESTAMP(0) NOT NULL,
-    "deletedAt" TIMESTAMP(0) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "settings_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "galerys" (
-    "id" UUID NOT NULL,
-    "invitationId" VARCHAR(255) NOT NULL,
-    "imageUrl" VARCHAR(255) NOT NULL,
-    "createdAt" TIMESTAMP(0) NOT NULL,
-    "updatedAt" TIMESTAMP(0) NOT NULL,
-    "deletedAt" TIMESTAMP(0) NOT NULL,
+    "id" TEXT NOT NULL,
+    "invitationId" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "galerys_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Users" (
-    "id" UUID NOT NULL,
-    "username" VARCHAR(255) NOT NULL,
-    "email" VARCHAR(255) NOT NULL,
-    "password" VARCHAR(255) NOT NULL,
-    "role" VARCHAR(255) NOT NULL,
-    "createdAt" TIMESTAMP(0) NOT NULL,
-    "updatedAt" TIMESTAMP(0) NOT NULL,
-    "deletedAt" TIMESTAMP(0) NOT NULL,
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "reviews" (
-    "id" UUID NOT NULL,
-    "invitationId" VARCHAR(255) NOT NULL,
+    "id" TEXT NOT NULL,
+    "invitationId" TEXT NOT NULL,
     "rate" BIGINT NOT NULL,
     "comment" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(0) NOT NULL,
-    "updatedAt" TIMESTAMP(0) NOT NULL,
-    "deletedAt" TIMESTAMP(0) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "reviews_pkey" PRIMARY KEY ("id")
 );
