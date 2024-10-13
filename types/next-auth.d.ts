0079f0a901de68705ef488f468318e04af3c7d91
Al-Fathir <118950136+Alfthrpy@users.noreply.github.com>
2024-10-13 10:16:54 +0700

    fix : missing field in session

---
+++ types/next-auth.d.ts
@@ -0,0 +1,14 @@
+// types/next-auth.d.ts
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import NextAuth from 'next-auth';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id?: string | null;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
+  }
+}
