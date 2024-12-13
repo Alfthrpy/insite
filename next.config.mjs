30555536ffd22ef3b70b7e39079a42567df4d5be
Al-Fathir <118950136+Alfthrpy@users.noreply.github.com>
2024-12-13 20:53:27 +0700

    coba debug lagi

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,22 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com', 'res.cloudinary.com', 'placehold.co'],
   },
+  experimental: {
+    middlewarePrefetch: 'flexible', // Memastikan middleware selalu dijalankan pada setiap request
+  },
+  async headers() {
+    return [
+      {
+        // Pastikan header berlaku untuk API atau path tertentu sesuai kebutuhan
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Disesuaikan dengan kebutuhan CORS
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,POST" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+        ],
+      },
+    ];
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.(mp3|wav|ogg)$/,
@@ -13,4 +29,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
